de69d7ce1e9ab463e52005ba3e5c04a01fc8bd01
feby25252 <biruusenjaa@gmail.com>
2026-04-16 21:56:42 +0700

    first commit

---
+++ backend/config/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// test route dulu biar aman
+app.get('/', (req, res) => {
+  res.send('API jalan');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
